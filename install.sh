670c0e843d3fa0383f85404c869dd76cce54a980
猫餌 <tuna@ijikechan.com>
2016-07-18 23:14:15 +0900

    font format change

--- install.sh
+++ install.sh
@@ -1,45 +1,46 @@
 #! /bin/bash
 if [ -d ~/dotfiles ]; then
-    if [ ! -e ~/.vimrc ]; then
-    ln -s ~/dotfiles/.vimrc ~/.vimrc
-    if [ ! -e ~/.vim ]; then
-    ln -s ~/dotfiles/.vim ~/.vim
-    if [ ! -e ~/.config ]; then
-    ln -s ~/dotfiles/.config ~/.config
-    if [ ! -e ~/.tmux.conf ]; then
-    ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-    if [ ! -e ~/.viminfo ]; then
-    ln -s ~/dotfiles/.viminfo ~/.viminfo
-    if [ ! -e ~/.vimperator ]; then
-    ln -s ~/dotfiles/.vimperator ~/.vimperator
-    if [ ! -e ~/.vimperatorrc ]; then
-    ln -s ~/dotfiles/.vimperatorrc ~/.vimperatorrc
-    if [ ! -e ~/.zshrc ]; then
-    ln -s ~/dotfiles/.zshrc ~/.zshrc
+if [ ! -e ~/.vimrc ]; then
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+elif [ ! -e ~/.vim ]; then
+ln -s ~/dotfiles/.vim ~/.vim
+elif [ ! -e ~/.config ]; then
+ln -s ~/dotfiles/.config ~/.config
+elif [ ! -e ~/.tmux.conf ]; then
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+elif [ ! -e ~/.viminfo ]; then
+ln -s ~/dotfiles/.viminfo ~/.viminfo
+elif [ ! -e ~/.vimperator ]; then
+ln -s ~/dotfiles/.vimperator ~/.vimperator
+elif [ ! -e ~/.vimperatorrc ]; then
+elln -s ~/dotfiles/.vimperatorrc ~/.vimperatorrc
+elif [ ! -e ~/.zshrc ]; then
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+fi
 else
-    mkdir ~/dotfiles 
+mkdir ~/dotfiles 
 fi
 
 if [ -e /usr/local/bin/brew ]; then
-    if [ ! -e /usr/local/bin/htop ]; then
-        brew install htop
-    elif [ ! -e /usr/local/bin/rbenv ]; then
-        brew install rbenv
-    elif [ ! -e /usr/local/bin/fish ]; then
-        brew install fish
-    elif [ ! -e /usr/local/bin/tig ]; then
-        brew install tig
-    elif [ ! -e /usr/local/bin/git ]; then
-        brew install git
-    elif [ ! -e /usr/local/bin/vim ]; then
-        brew install vim
-        brew install tidy-html5
-    elif [ ! -e /usr/local/bin/nodenv ]; then
-        brew install nodenv
-    elif [ ! -e /usr/local/bin/tmux ]; then
-        brew install tmux
-        brew install reattach-to-user-namespace
-    fi
+if [ ! -e /usr/local/bin/htop ]; then
+brew install htop
+elif [ ! -e /usr/local/bin/rbenv ]; then
+brew install rbenv
+elif [ ! -e /usr/local/bin/fish ]; then
+brew install fish
+elif [ ! -e /usr/local/bin/tig ]; then
+brew install tig
+elif [ ! -e /usr/local/bin/git ]; then
+brew install git
+elif [ ! -e /usr/local/bin/vim ]; then
+brew install vim
+brew install tidy-html5
+elif [ ! -e /usr/local/bin/nodenv ]; then
+brew install nodenv
+elif [ ! -e /usr/local/bin/tmux ]; then
+brew install tmux
+brew install reattach-to-user-namespace
+fi
 else
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
