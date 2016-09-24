9805fd4b98b4072186560153a777a23db849d1be
JpnLavender <nagisa20000014@gmail.com>
2016-09-24 15:10:52 +0900

    add case at linux config

--- install.sh
+++ install.sh
@@ -17,9 +17,37 @@ fi
 [ -e  ~/.tmux/plugins/tpm ] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 
-if [ ! $(which brew) ]; then
-    brew tap homebrew/brewdler
-    brew brewdle
-else
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+case ${OSTYPE} in
+    darwin*)
+        if [ ! $(which brew) ]; then
+            brew tap homebrew/brewdler
+            brew brewdle
+        else
+            /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        fi
+        #fish install
+        shell_path=`which fish`
+        chsh -s $shell_path
+        #ruby install
+        rbenv install 2.3.1
+        ;;
+    linux*)
+        #System install
+        sudo su -
+        curl http://fishshell.com/files/linux/RedHat_RHEL-5/fish.release:2.repo > /etc/yum.repos.d/shells:fish:release:2.repo #fish install Package
+        yum install tig fish htop tmux vim
+        shell_path=`which fish`
+        chsh -s $shell_path
+        #Ruby install 
+        yum -y install gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel libffi-devel libxml2 libxslt libxml2-devel libxslt-devel sqlite-devel
+        git clone https://github.com/sstephenson/rbenv.git /usr/local/rbenv
+        cp -p /etc/profile /etc/profile.ORG
+        diff /etc/profile /etc/profile.ORG
+        echo 'export RBENV_ROOT="/usr/local/rbenv"' >> /etc/profile
+        echo 'export PATH="${RBENV_ROOT}/bin:${PATH}"' >> /etc/profile
+        echo 'eval "$(rbenv init -)"' >> /etc/profile
+        source /etc/profile
+        git clone https://github.com/sstephenson/ruby-build.git /usr/local/rbenv/plugins/ruby-build
+        rbenv install 2.3.1
+        ;;
+esac
