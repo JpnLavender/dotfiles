a5d550143334a79c5b419fdb366a20d44fde1a69
JpnLavender <nagisa20000014@gmail.com>
2016-09-28 11:57:02 +0900

    Add alias & peco

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,3 +20,17 @@ alias c='cd'
 alias r='ruby'
 
 alias dl='~/Desktop/AllFile/Programming/youtube-dl/youtube_dl.sh'
+
+alias illust='ssh -i "irimameMac.pem" ec2-user@ec2-54-199-161-164.ap-northeast-1.compute.amazonaws.com'
+
+alias server='ssh -i "irimameMac.pem" ec2-user@ec2-54-199-205-214.ap-northeast-1.compute.amazonaws.com'
+
+# alias docker_rmi='docker rm `docker ps -a -q` & docker images | awk '/^<none>/ { print $3 }' | xargs docker rmi'
+
+function fish_user_key_bindings
+  bind \c] peco_history # Bind for prco change directory to Ctrl+]
+end
+
+function peco_history
+	history | peco | read line; commandline $line
+end
