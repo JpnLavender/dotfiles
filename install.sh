011d39962d8a55ec405f14ddbac2d2c0553210b6
JpnLavender <irimamekun@gmail.com>
2017-01-27 15:04:30 +0900

    zshようの設定ファイルを手軽にlnできるようにと、tmux-powerlineのインストールようの記述を追加

--- install.sh
+++ install.sh
@@ -8,15 +8,22 @@ if [ ! -e ~/.vim ]; then
         ln -s ~/dotfiles/.vim/.vimrc.$file ~/.vim/.vimrc.$file
     done
 fi
-[ -e ~/.zshrc ]             || ln -s ~/dotfiles/.zshrc ~/.zshrc
-[ -e ~/.config ]            || ln -s ~/dotfiles/.config ~/.config
-[ -e ~/.viminfo ]           || ln -s ~/dotfiles/.viminfo ~/.viminfo
-[ -e ~/.tmux.conf ]         || ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-[ -e ~/.vimperator ]        || ln -s ~/dotfiles/.vimperator ~/.vimperator
-[ -e ~/.vimperatorrc ]      || ln -s ~/dotfiles/.vimperatorrc ~/.vimperatorrc
-[ -e  ~/.tmux-powerline ]   || ln -s ~/dotfiles/tmux-powerline/ ~/tmux-powerline
-[ -e  ~/.tmux/plugins/tpm ] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+[ -e ~/.zshrc ] || ln -s ~/dotfiles/.zshrc ~/.zshrc
+if [ ! -e ~/.zsh ]; then
+    ln -s ~/dotfiles/.zsh ~
+    files=( alias basic export function )
+    for file in ${files[@]}; do
+        ln -s ~/dotfiles/.zsh/$file.zsh ~/.zsh/$file.zsh
+    done
+fi
+
+[ -e  ~/.tmux-powerline ]                      || git clone https://github.com/erikw/tmux-powerline.git ~/tmux-powerline
+[ -e  ~/.tmux/plugins/tpm ]                    || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+[ -e ~/.tmux.conf ]                            || ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+[ -e ~/.vimperator ]                           || ln -s ~/dotfiles/.vimperator ~/.vimperator
+[ -e ~/.vimperatorrc ]                         || ln -s ~/dotfiles/.vimperatorrc ~/.vimperatorrc
+[ -e ~/tmux-powerline/themes/default.sh ]      || ln -s ~/dotfiles/default.sh ~/tmux-powerline/themes/default.sh
 
 case ${OSTYPE} in
     darwin*)
@@ -26,26 +33,12 @@ case ${OSTYPE} in
         else
             /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
         fi
-        #fish install
-        shell_path=`which fish`
+        shell_path=`which zsh`
         chsh -s $shell_path
         ;;
     linux*)
-        #System install
-        sudo curl http://fishshell.com/files/linux/RedHat_RHEL-5/fish.release:2.repo > /etc/yum.repos.d/shells:fish:release:2.repo #fish install Package
-        sudo yum install tig fish htop tmux vim
-        shell_path=`which fish`
+        sudo yum install -y tig fish htop tmux vim zsh
+        shell_path=`which zsh`
         sudo chsh -s $shell_path
-        #Ruby install 
-        sudo yum -y install gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel libffi-devel libxml2 libxslt libxml2-devel libxslt-devel sqlite-devel
-        git clone https://github.com/sstephenson/rbenv.git /usr/local/rbenv
-        sudo cp -p /etc/profile /etc/profile.ORG
-        sudo diff /etc/profile /etc/profile.ORG
-        echo 'export RBENV_ROOT="/usr/local/rbenv"' >> /etc/profile
-        echo 'export PATH="${RBENV_ROOT}/bin:${PATH}"' >> /etc/profile
-        echo 'eval "$(rbenv init -)"' >> /etc/profile
-        sudo source /etc/profile
-        git clone https://github.com/sstephenson/ruby-build.git /usr/local/rbenv/plugins/ruby-build
-        rbenv install 2.3.1
         ;;
 esac
