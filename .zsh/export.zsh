74c8297bf9a27c0edf59542b2e0a2e8b62c38dc5
JpnLavender <irimamekun@gmail.com>
2017-01-27 15:02:09 +0900

    export PATHの記述を追加

--- .zsh/export.zsh
+++ .zsh/export.zsh
@@ -8,7 +8,7 @@ source $ZSH/oh-my-zsh.sh
 
 export TERM="xterm-256color"
 
-source .bashrc
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:~/.local/bin
 
 powerline-daemon -q
 . /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
